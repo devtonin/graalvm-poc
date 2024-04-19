67d1b1fb168ee64f4a270415206d2dfc55ea374f
antonioamaral <antonio.amaral@sensedia.com>
2024-04-19 15:50:46 -0300

    poc runtime graalvm

---
+++ src/test/java/com/devtonin/graalvmpoc/GraalvmPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtonin.graalvmpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
